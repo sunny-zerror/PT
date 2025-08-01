1de9865173d3b15f4db7f346e189db0a6eb5002c
sunny-zerror <sunny.zerror@gmail.com>
2025-08-01 20:45:22 +0530

    footer

--- src/components/sections/FrameA.jsx
+++ src/components/sections/FrameA.jsx
@@ -286,6 +286,17 @@ const FrameA = () => {
         updatePercent();
     }, []);
 
+    useEffect(() => {
+        gsap.to(".rotate", {
+            rotation: 360,
+            ease: "none",
+            duration: 10,
+            repeat: -1,
+            transformOrigin: "center center",
+        })
+
+    }, [])
+
     return (
         <div>
             <div ref={main_Ref} className="h-screen w-full relative">
@@ -537,10 +548,28 @@ const FrameA = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
-        </div>
-    );
-};
+            <div className="w-full h-[20vh] p-10  flex items-center justify-between">
+                <div className="w-[30%] h-full">
+                    <p>
+                        I’m open to collaborations, commissions, or a good conversation. <br />
+                        Email me at <b>pirantarapora@gmail.com</b>
+                    </p>
+                </div>
+                <div className="w-[30%] h-full flex justify-center items-center gap-5">
+                    <img src="/logos/insta.svg" alt="" />
+                    <img src="/logos/linkedin.svg" alt="" />
+                </div>
+                <div className="w-[30%] flex items-center gap-10 justify-center h-full">
+                    <p>Copyright © 2025 . All rights reserved.</p>
+                    <div className="center group">
+                        <img className='absolute group-hover:-rotate-45 z-[9] transition-all duration-300' src="/logos/arrow-right.svg" alt="" />
+                        <img className='rotate' src="/logos/contact_rotate.svg" alt="" />
+                    </div>
+                </div>
+            </div>
+                </div>
+    );  
+};  
 
 export default FrameA;
--- src/pages/demo/index.jsx
+++ src/pages/demo/index.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import { RiInstagramLine, RiLinkedinLine } from '@remixicon/react'
+import gsap from 'gsap'
+import React, { useEffect } from 'react'
 
-const Index = () => {
+const index = () => {
+  useEffect(() => {
+
+    gsap.to(".rotate", {
+      rotation: 360,
+      ease: "none",
+      duration: 10,
+      repeat: -1,
+      transformOrigin: "center center",
+    })
+
+  }, [])
+
   return (
     <div>
-      <div className="w-full h-screen grid grid-cols-[70%_30%]">
-        <div className="bg-red-500">Column 1 (70%)</div>
-        <div className="bg-blue-500">Column 2 (30%)</div>
+      <div className="w-full h-[20vh] p-10  flex items-center justify-between">
+        <div className="w-[30%] h-full">
+          <p>
+            I’m open to collaborations, commissions, or a good conversation. <br />
+            Email me at <b>pirantarapora@gmail.com</b>
+          </p>
+        </div>
+        <div className="w-[30%] h-full flex justify-center items-center gap-5">
+          <img src="/logos/insta.svg" alt="" />
+          <img src="/logos/linkedin.svg" alt="" />
+        </div>
+        <div className="w-[30%] flex items-center gap-10 justify-center h-full">
+          <p>Copyright © 2025 . All rights reserved.</p>
+          <div className="center group">
+            <img className='absolute group-hover:-rotate-45 z-[9] transition-all duration-300' src="/logos/arrow-right.svg" alt="" />
+            <img className='rotate' src="/logos/contact_rotate.svg" alt="" />
+          </div>
+        </div>
       </div>
     </div>
   )
 }
 
-export default Index
+export default index
